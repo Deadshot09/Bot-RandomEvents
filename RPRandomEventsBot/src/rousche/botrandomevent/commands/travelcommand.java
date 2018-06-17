f36019116a980eeb710d0206ad0e5ee0c631b509
Sonnicon <sonniconn@gmail.com>
2018-06-17 19:33:25 +0100

    Travel args

--- RPRandomEventsBot/src/rousche/botrandomevent/commands/travelcommand.java
+++ RPRandomEventsBot/src/rousche/botrandomevent/commands/travelcommand.java
@@ -13,11 +13,18 @@ public class travelcommand implements Commands {
     public void action(String args[] , MessageReceivedEvent e){
         String event = "";
         int random = (int) (Math.random() * ((100 - 1)+1)) + 1;
-        if (random <= 50){
-            event = "You hear wolves howling somewhere close behind you. You look behind and see you see a pack of direwolves sprinting toward you !";
-        }
-        else {
-            event = "You can hear the sounds of a raging battle in front of you. You hurry toward the sounds and there you see a caravan being attacked by bandits!";
+        if(args[0].toLowerCase().equals("forest")) {
+            if (random <= 50) {
+                event = "You hear wolves howling somewhere close behind you. You look behind and see you see a pack of direwolves sprinting toward you !";
+            } else {
+                event = "You can hear the sounds of a raging battle in front of you. You hurry toward the sounds and there you see a caravan being attacked by bandits!";
+            }
+        }else if (args[0].toLowerCase().equals("sea")){
+            if (random <= 50) {
+                event = "Your mast gets snapped by the almighty kraken!";
+            } else {
+                event = "You see a ship getting raided by pirates in the distance!";
+            }
         }
         e.getTextChannel().sendMessage(event).queue();
     }
